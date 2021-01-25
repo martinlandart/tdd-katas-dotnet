90f67ac8098475d4ea46beb9a65ff15d7eaa6fee
Martín Landart <martinjl95@hotmail.com>
2021-01-24 21:29:10 -0300

    PrimeFactors WIP

---
+++ TDD Katas/Katas/PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TDD_Katas.Katas.PrimeFactors
+{
+    public class PrimeFactors
+    {
+        public IEnumerable<int> Generate(int input)
+        {
+            return new List<int>() { input };
+        }
+    }
+}
---
+++ Tests/TDD.Katas.UnitTests/PrimeFactorsTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using TDD_Katas.Katas.PrimeFactors;
+using Xunit;
+
+namespace TDD.Katas.UnitTests
+{
+    public class PrimeFactorsTests
+    {
+        private readonly PrimeFactors pf;
+
+        public PrimeFactorsTests()
+        {
+            pf = new PrimeFactors();
+        }
+
+        [Fact]
+        public void GenerateOf1ShouldReturn1()
+        {
+            pf.Generate(1).Should().BeEquivalentTo(new List<int>() { 1 });
+        }
+
+        [Fact]
+        public void GenerateOf2ShouldReturn2()
+        {
+            pf.Generate(2).Should().BeEquivalentTo(new List<int>() { 2 }); ;
+        }
+
+        [Fact]
+        public void GenerateOf3ShouldReturn3()
+        {
+            pf.Generate(3).Should().BeEquivalentTo(new List<int>() { 3 });
+        }
+    }
+}
