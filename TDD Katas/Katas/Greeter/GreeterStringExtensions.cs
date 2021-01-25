42277996d6d43cd227bf365ff1a5e2d29a823c4d
Martín Landart <martinjl95@hotmail.com>
2021-01-24 21:58:07 -0300

    Revert "Remove unused file"
    This reverts commit 1250f523ee263180efa5376c986c5fe702f3af57.

---
+++ TDD Katas/Katas/Greeter/GreeterStringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace TDD.Katas.GreeterKata
+{
+    public static class GreeterStringExtensions
+    {
+        public static string ToTitleCase(this string value)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            return textInfo.ToTitleCase(value);
+        }
+
+        public static string Prepend(this string value, string greetingMessage)
+        {
+            return $"{greetingMessage} {value}";
+        }
+    }
+}
